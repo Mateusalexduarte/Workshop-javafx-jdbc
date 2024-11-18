29886abde828e5701f647c62681fe44716c3ad61
Mateus Duarte <mateusalex023@gmail.com>
2024-11-18 10:29:50 -0300

    Saving a new Department

--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -4,10 +4,22 @@ import javafx.event.ActionEvent;
 import javafx.scene.Node;
 import javafx.stage.Stage;
 
-public interface Utils {
+public class Utils {
 	
 	public static Stage currentStage(ActionEvent event) {
 		return (Stage) ((Node) event.getSource()).getScene().getWindow();
 	}
 
+	
+	public static Integer tryParsetoInt(String str) {
+		try {
+		return Integer.parseInt(str);
+		}
+		catch(NumberFormatException e) {
+			return null;
+		}
+	}
+	
+	
+	
 }
